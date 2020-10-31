d259db3f25ecf90ade3e3cd266b784f8bb569f36
hedeencharles <hedeencharles@gmail.com>
2020-10-31 10:30:31 -0700

    updates to mongo code

--- Flask (Group_Project_2)/simple_app.py
+++ Flask (Group_Project_2)/simple_app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask import render_template
+import pymongo
 
-# Establish connection to local host 27017
-conn = 'mongodb://localhost:27017'
+# # Establish connection to local host 27017
+# conn = 'mongodb://localhost:27017'
 
-# Connect to MongoClient
-client = pymongo.MongoClient(conn)
+# # Connect to MongoClient
+# client = pymongo.MongoClient(conn)
 
 
 
@@ -15,7 +16,7 @@ client = pymongo.MongoClient(conn)
 app = Flask(__name__)
 
 #Create the Flask route for the index page
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template("index.html")
 
@@ -24,33 +25,33 @@ def index():
 def about():
     return render_template("about.html")
 
-# app route for api
-@app.route("/api/v1.0/nba_player_info")
-def players():
-    """Return player data as json"""
-    # Create session link for Python to DB
-    query = db.testing_beds.find_one()
+# # app route for api
+# @app.route("/api/v1.0/nba_player_info")
+# def players():
+#     """Return player data as json"""
+#     # Create session link for Python to DB
+#     query = db.testing_beds.find_one()
 
 
-#Create the Flask route for the data page
-@app.route('/data', methods=['GET'])
-def data():
-    return '''<h1>NBA Player Birth Data</h1>
-<p>API for referencing NBA Player Birthplaces.</p>'''
+# #Create the Flask route for the data page
+# @app.route('/data', methods=['GET'])
+# def data():
+#     return '''<h1>NBA Player Birth Data</h1>
+# <p>API for referencing NBA Player Birthplaces.</p>'''
 
-@app.route('/api/v1.0/player_data')
-def (player_data)
+# @app.route('/api/v1.0/player_data')
+# def (player_data)
 
 # @app.route('/data', methods=['GET'])
 # def api_all():
 #     return json.dumps(playerdata)
 
 
-    # Use a for loop to confirm data is now located in the database
-    all_players = []
-    for player in query:
-        all_players.append(player)
+    # # Use a for loop to confirm data is now located in the database
+    # all_players = []
+    # for player in query:
+    #     all_players.append(player)
 
-    return jsonify(all_results)
+    # return jsonify(all_results)
     
 app.run(debug=True, port=5000)
